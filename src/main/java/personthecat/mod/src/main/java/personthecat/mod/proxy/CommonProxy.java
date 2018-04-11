1f1cc35a97e233109a64efce36fcbf82d2cfce72
PersonTheCat <33143148+PersonTheCat@users.noreply.github.com>
2018-04-11 13:12:42 -0500

    Damn GitHub

--- src/main/java/personthecat/mod/src/main/java/personthecat/mod/proxy/CommonProxy.java
+++
@@ -1,9 +0,0 @@
-package personthecat.mod.proxy;
-
-import net.minecraft.item.Item;
-
-public class CommonProxy 
-{	
-	public void registerItemRenderer(Item item, String filename) {}
-	public void registerVariantRenderer(Item item, int meta, String filename) {}
-}
