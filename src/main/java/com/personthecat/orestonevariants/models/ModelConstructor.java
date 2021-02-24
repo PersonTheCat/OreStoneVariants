b7e4ce31d83b7afaf7f1134c4162ebcb44b29afd
PersonTheCat <professionallyperson@gmail.com>
2021-02-24 09:58:35 -0600

    Document complicated class

--- src/main/java/com/personthecat/orestonevariants/models/ModelConstructor.java
+++ src/main/java/com/personthecat/orestonevariants/models/ModelConstructor.java
@@ -60,6 +60,11 @@ public class ModelConstructor {
     private static final String MODEL_TEMPLATE = getResourceAsString(MODEL_TEMPLATE_PATH)
         .orElseThrow(() -> runEx("Couldn't find model template"));
 
+    /**
+     *  Constructs the overlay model to be used by <em>all</em> of the ore variants. The
+     * generated file can be edited by the user, but it will come with defaults which are
+     * ultimately defined in the config file.
+     */
     public static void generateOverlayModel() {
         if (!resourceExists(OVERLAY_MODEL_PATH)) {
             final double overlayOffset = ((Cfg.modelScale.get() * 16.0) - 16.0) / 2.0;
@@ -72,7 +77,25 @@ public class ModelConstructor {
         }
     }
 
-    // Generates a new set of block models from fg and bg with osv state properties.
+    /**
+     *  Generates a new set of block models from an ore property type, a background block,
+     * and any extra states defined in <code>textures</code>, as these are individually
+     * mapped to different textures.
+     *
+     * <p>This operation will construct concrete models for the following entities:
+     *
+     * <ul>
+     *   <li>Block state files</li>
+     *   <li>Block models</li>
+     *   <li>Item models</li>
+     * </ul>
+     *
+     *  These models will not be generated if they already exist, and thus the user can
+     * edit any individual model component without changes being overwritten.
+     *
+     * @param variant The block to generate models for. Only {@link BaseOreVariant} types
+     *                contain all of the needed information.
+     */
     public static void generateModels(BaseOreVariant variant) {
         final OreProperties properties = variant.properties;
         final Block bg = variant.bgState.getBlock();
@@ -81,12 +104,38 @@ public class ModelConstructor {
         generateModels(id, properties, bg);
     }
 
+    /**
+     * Variant of {@link #generateModels(BaseOreVariant)} which accepts the raw information
+     * needed for constructing block variants, block states, and item models.
+     *
+     * @param id The location of the <em>OSV</em> block.
+     * @param properties Data containing texture info and clues for generating model names.
+     * @param bg The background block which our generated models are based on.
+     */
     private static void generateModels(ResourceLocation id, OreProperties properties, Block bg) {
         final JsonObject variants = generateAllVariants(properties, bg);
         generateBlockStateFile(id, variants);
         generateItemModels(id, properties, variants, bg);
     }
 
+    /**
+     *  This function is responsible for generating all of the block model variants which
+     * will be needed for the current ore. Variants are based on all combinations of the
+     * following data:
+     *
+     * <ul>
+     *   <li>Background variants</li>
+     *   <li>Texture variants (not foreground variants)</li>
+     *   <li>Dense state (true or false)</li>
+     * </ul>
+     *
+     *  <p>This function will record and return data about the models generated in the form of
+     * a JSON object which can be used to construct a new block state file.
+     *
+     * @param properties Data containing texture info and clues for generating model names.
+     * @param bg The background block which our generated models are based on.
+     * @return The contents of <code>variants</code> to go in a new block state file.
+     */
     private static JsonObject generateAllVariants(OreProperties properties, Block bg) {
         final JsonObject variants = new JsonObject();
 
@@ -100,17 +149,39 @@ public class ModelConstructor {
         return variants;
     }
 
+    /**
+     * This function generates a new block state file at the given resource location. A new
+     * model will not be generated if one already exists.
+     *
+     * @param id The location where this model will be written (in /blockstates)
+     * @param variants The JSON contents of <code>variants</code>.
+     */
     private static void generateBlockStateFile(ResourceLocation id, JsonObject variants) {
         final String concretePath = getBlockStatePath(id);
 
         if (!resourceExists(concretePath)) {
-            final String blockState = new JsonObject()
-                .add(VARIANTS_KEY, compressArrays(variants))
-                .toString(Stringify.FORMATTED);
-            ResourceHelper.writeResource(concretePath, blockState).ifErr(Result::WARN);
+            final JsonObject blockState = new JsonObject()
+                .add(VARIANTS_KEY, compressArrays(variants));
+            writeJson(concretePath, blockState);
         }
     }
 
+    /**
+     *  This function generates normal and dense variant items for the block corresponding to
+     * <code>id</code>. An existing block model will be reused as the parent model for these
+     * items. The logic works like this:
+     *
+     * <ol>
+     *   <li>Read the item model for <code>bg</code> and get its parent model, if possible.</li>
+     *   <li>Look for an equivalent OSV block model for the given <code>properties</code>.</li>
+     *   <li>If an OSV model exists, use it. If not, get the first normal model in <code>variants</code>.</li>
+     * </ol>
+     *
+     * @param id The location where this model will be written (in /models/item)
+     * @param properties Data containing clues about which model to reuse.
+     * @param variants All of the regular block variants, if needed.
+     * @param bg The background block which these models are based on.
+     */
     private static void generateItemModels(ResourceLocation id, OreProperties properties, JsonObject variants, Block bg) {
         final String normalPath = getItemModelPath(id);
         final String densePath = PathTools.ensureDense(normalPath);
@@ -127,6 +198,15 @@ public class ModelConstructor {
         }
     }
 
+    /**
+     * This function places every possible OSV state inside of a single background state.
+     * Changes will be recorded inside of <code>variants</code>.
+     *
+     * @param properties Data used for generating the foreground model name.
+     * @param variants All of the regular block variants being written into.
+     * @param bgKey The current background state being reused.
+     * @param base The base model corresponding to this background state.
+     */
     private static void addVariants(OreProperties properties, JsonObject variants, String bgKey, JsonObject base) {
         final MultiValueMap<String, ResourceLocation> textures = properties.texture.overlayLocations;
 
@@ -145,7 +225,23 @@ public class ModelConstructor {
         }
     }
 
-    // Generates block state keys, ignoring empty properties ("": {...}).
+    /**
+     *  Generates a block state key from multiple states, matching the vanilla syntax in
+     * 1.16. This will ignore empty properties, e.g. (<code>"": {...}</code>).
+     *
+     * <p>For example, given the following state values:
+     *
+     * <ul>
+     *   <li><code>axis=x</code></li>
+     *   <li><code>oxidation=0</code></li>
+     *   <li><code>dense=false</code></li>
+     * </ul>
+     *
+     * The following key will be generated: <code>axis=x,oxidation=0,dense=false</code>
+     *
+     * @param values All of the state properties being reused.
+     * @return A new key containing these properties.
+     */
     private static String generateKey(String... values) {
         final StringBuilder sb = new StringBuilder();
         for (String property : values) {
@@ -159,6 +255,15 @@ public class ModelConstructor {
         return sb.toString();
     }
 
+    /**
+     * Generates a <em>single</em> block state variant and block model.
+     *
+     * @param properties Data used for generating model locations.
+     * @param base The variant data from the background block state file.
+     * @param texture The overlay resource location, as a string.
+     * @param dense Whether to generate the dense or regular variant.
+     * @return All of the generated variant data, which will go in the block state file.
+     */
     private static JsonObject generateVariant(OreProperties properties, JsonObject base, String texture, boolean dense) {
         final JsonObject variant = new JsonObject();
 
@@ -176,6 +281,16 @@ public class ModelConstructor {
         return variant;
     }
 
+    /**
+     * Generates a new block model, otherwise returns the resource location of the model.
+     * The model is guaranteed to exist at this point.
+     *
+     * @param properties Data used for generating the model location.
+     * @param bgModel The resource location of the background model, as a string.
+     * @param texture The overlay resource location, as a string.
+     * @param dense Whether to generate the dense or regular variant.
+     * @return The resource location of the generated model, as a string.
+     */
     private static String loadOrGenerateModel(OreProperties properties, String bgModel, String texture, boolean dense) {
         final String path = foreignModelToOSV(properties, bgModel, dense);
         final String concretePath = getModelPath(path);
@@ -186,6 +301,13 @@ public class ModelConstructor {
         return path;
     }
 
+    /**
+     * Attempts to locate an equivalent OSV block model from a foreign item model.
+     *
+     * @param properties Data used for generating the model location.
+     * @param item The parsed item model of the background block.
+     * @return The resource location of the <em>OSV</em> block model, if possible.
+     */
     private static Optional<String> getEquivalentFromItem(OreProperties properties, JsonObject item) {
         final String parent = item.getString(PARENT_KEY, "");
         final String equivalent = foreignModelToOSV(properties, parent, false);
@@ -194,6 +316,17 @@ public class ModelConstructor {
         return resourceExists(concrete) ? full(equivalent) : empty();
     }
 
+    /**
+     *  Gets the first model from a <code>variants</code> object which is not dense.
+     *
+     *  Note that this requires the key <code>dense=false</code> and will not work if that
+     * is removed. This key <em>should not be removed</em>, as this has been tested and does
+     * not work as expected. Thus, its existence here is a lazy assertion that the variant
+     * keys are generated correctly.
+     *
+     * @param variants The <code>variants</code> object from the block state file.
+     * @return The first normal model resource location, as a string.
+     */
     private static String getFirstNormalModel(JsonObject variants) {
         for (JsonObject.Member variant : variants) {
             final JsonValue value = variant.getValue();
@@ -205,23 +338,49 @@ public class ModelConstructor {
         throw runEx("Generated block state contains no variants");
     }
 
+    /**
+     * Converts the model path of a <em>foreign</em> block (i.e. not an OSV block) into
+     * the equivalent OSV model path.
+     *
+     * @param properties Data containing foreground info which will be used in the path.
+     * @param from The original model resource location, as a string.
+     * @param dense Whether to use the dense model path.
+     * @return The OSV model resource location, as a string.
+     */
     private static String foreignModelToOSV(OreProperties properties, String from, boolean dense) {
         final String path = PathTools.namespaceToSub(from);
         final String prefix = (dense ? "dense_" : "") + properties.name + "_";
         return f("{}:{}", Main.MODID, PathTools.prependFilename(path, prefix));
     }
 
+    /**
+     * Converts the <code>MODEL_TEMPLATE</code> from the jar into a new block model.
+     *
+     * @param bgModel The original model's resource location, as a string.
+     * @param texture The overlay texture's resource location, as a string.
+     * @return The raw JSON model data, as a string.
+     */
     private static String generateModel(String bgModel, String texture) {
         return MODEL_TEMPLATE.replace(BG_KEY, bgModel)
             .replace(FG_KEY, texture)
             .replace(LAYER_KEY, getFgType());
     }
 
+    /**
+     * Determines whether to use the translucent render layer or the cutout mipped layer.
+     *
+     * @return The key of the desired layer, as indicated in the config file.
+     */
     private static String getFgType() {
         return Cfg.translucentTextures.get() ? TRANSLUCENT_TYPE : CUTOUT_MIPPED_TYPE;
     }
 
-    /** Replaces any single element arrays at the top level with regular values. */
+    /**
+     * Replaces any single element arrays at the top level with regular values.
+     *
+     * @param json Any regular JSON file.
+     * @return The updated JSON data.
+     */
     private static JsonObject compressArrays(JsonObject json) {
         final JsonObject clone = new JsonObject();
         for (JsonObject.Member member : json) {
@@ -237,50 +396,119 @@ public class ModelConstructor {
         return clone;
     }
 
+    /**
+     * Writes a new JSON object to the resources directory as a formatted string.
+     *
+     * @param concretePath The raw, relative path to this model.
+     * @param json The JSON object being serialized.
+     */
     private static void writeJson(String concretePath, JsonObject json) {
         writeJson(concretePath, json.toString(Stringify.FORMATTED));
     }
 
+    /**
+     * Writes a string to the resources directory.
+     *
+     * @param concretePath The raw, relative path to this file.
+     * @param json Any string data (which will always be a JSON).
+     */
     private static void writeJson(String concretePath, String json) {
         ResourceHelper.writeResource(concretePath, json).ifErr(Result::WARN);
     }
 
+    /**
+     * Attempts to load the <code>variants</code> object from a block state file.
+     *
+     * Note that there is a design flaw here: multipart blocks are <em>not</em> supported.
+     *
+     * @param block The block being researched.
+     * @return The <code>variants</code> data, or an empty JSON object.
+     */
     private static JsonObject loadVariantsOrEmpty(Block block) {
         return loadBlockStateFile(block)
             .flatMap(json -> getObject(json, VARIANTS_KEY))
             .orElseGet(JsonObject::new);
     }
 
-    // This is possible now that all states use the same mapper.
+    /**
+     * Attempts to locate the block state corresponding to the given block.
+     *
+     * @param block The background block being researched.
+     * @return The entire block state file as a JSON object, if possible.
+     */
     private static Optional<JsonObject> loadBlockStateFile(Block block) {
         return loadJson(getBlockStatePath(block));
     }
 
+    /**
+     * Attempts to locate the main item model corresponding to the given block.
+     *
+     * @param block The background block being researched.
+     * @return The entire item model as a JSON object, if possible.
+     */
     private static Optional<JsonObject> loadItemModel(Block block) {
         return loadJson(getItemModelPath(block));
     }
 
+    /**
+     * Attempts to locate the block state file corresponding to the given block.
+     *
+     * This is only possible now that all states use the same mapper.
+     *
+     * @param block The background block being researched.
+     * @return The path to the block state file.
+     */
     private static String getBlockStatePath(Block block) {
         return getBlockStatePath(Objects.requireNonNull(block.getRegistryName(), "Unregistered block"));
     }
 
+    /**
+     * Converts a resource location into a raw block state path.
+     *
+     * @param id The location of the desired block state file.
+     * @return The concrete location of this file.
+     */
     private static String getBlockStatePath(ResourceLocation id) {
         return f("assets/{}/blockstates/{}.json", id.getNamespace(), id.getPath());
     }
 
+    /**
+     * Converts a resource location into a raw model path.
+     *
+     * @param location The location of the desired model file.
+     * @return The concrete location of this file.
+     */
     private static String getModelPath(String location) {
         final ResourceLocation id = new ResourceLocation(location);
         return f("assets/{}/models/{}.json", id.getNamespace(), id.getPath());
     }
 
+    /**
+     * Attempts to locate the item model file corresponding to the given block.
+     *
+     * @param block The background block being researched.
+     * @return The path to the item model file.
+     */
     private static String getItemModelPath(Block block) {
         return getItemModelPath(Objects.requireNonNull(block.getRegistryName(), "Unregistered block"));
     }
 
+    /**
+     * converts a resource location into an <em>item</em> model file.
+     *
+     * @param id The location of the desired item model.
+     * @return The concrete location of this file.
+     */
     private static String getItemModelPath(ResourceLocation id) {
         return f("assets/{}/models/item/{}.json", id.getNamespace(), id.getPath());
     }
 
+    /**
+     * Attempts to load a file from the jar <b>or</b> resources directory as a JSON object.
+     *
+     * @param path The <em>concrete</em> path to this file.
+     * @return The deserialized JSON data from the disk, if possible.
+     */
     private static Optional<JsonObject> loadJson(String path) {
         return getResource(path).flatMap(HjsonTools::readJson);
     }
