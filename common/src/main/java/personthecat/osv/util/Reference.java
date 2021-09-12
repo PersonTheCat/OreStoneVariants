b7227f30e91586c4a2c8e4f86c5ed7ee54982128
PersonTheCat <professionallyperson@gmail.com>
2021-09-12 00:19:57 -0500

    Basic arch-loom project structure

---
+++ common/src/main/java/personthecat/osv/util/Reference.java
@@ -0,0 +1,5 @@
+package personthecat.osv.util;
+
+public class Reference {
+    public static final String MOD_ID = "osv";
+}
---
+++ platforms/forge/src/main/java/personthecat/osv/OSV.java
@@ -0,0 +1,7 @@
+package personthecat.osv;
+
+import net.minecraftforge.fml.common.Mod;
+import personthecat.osv.util.Reference;
+
+@Mod(Reference.MOD_ID)
+public class OSV {}
