5c14c030f71b1205cda064a80f2db2ab74e1447e
PersonTheCat <professionallyperson@gmail.com>
2021-10-28 09:25:42 -0500

    Temporarily rethrow graphical errors

--- common/src/main/java/personthecat/catlib/event/error/LibErrorContext.java
+++ common/src/main/java/personthecat/catlib/event/error/LibErrorContext.java
@@ -1,7 +1,9 @@
 package personthecat.catlib.event.error;
 
+import lombok.extern.log4j.Log4j2;
 import personthecat.catlib.exception.FormattedException;
 
+@Log4j2
 public class LibErrorContext {
 
     public static void registerSingle(final String modName, final FormattedException e) {
@@ -9,6 +11,7 @@ public class LibErrorContext {
     }
 
     public static void registerSingle(final Severity level, final String modName, final FormattedException e) {
-        throw new UnsupportedOperationException();
+        log.error("Graphical errors not yet supported. Throwing...");
+        throw new RuntimeException(e);
     }
 }
