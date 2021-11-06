56dc5e06a49e1c4e80313aa06807004c51dcc094
PersonTheCat <professionallyperson@gmail.com>
2021-11-05 19:55:15 -0500

    /debug features

--- common/src/main/java/personthecat/osv/command/CommandOsv.java
+++ common/src/main/java/personthecat/osv/command/CommandOsv.java
@@ -1,21 +1,40 @@
 package personthecat.osv.command;
 
+import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.feature.Feature;
 import org.apache.commons.lang3.mutable.MutableInt;
 import org.hjson.JsonValue;
 import personthecat.catlib.command.CommandContextWrapper;
 import personthecat.catlib.command.annotations.ModCommand;
+import personthecat.catlib.command.annotations.Node;
 import personthecat.catlib.io.FileIO;
 import personthecat.catlib.util.HjsonUtils;
+import personthecat.catlib.util.ResourceArrayLinter;
 import personthecat.osv.init.PresetLoadingContext;
 import personthecat.osv.io.ModFolders;
 import personthecat.osv.preset.data.OreSettings;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class CommandOsv {
 
     @ModCommand
-    private static void debugTest(final CommandContextWrapper ctx) {
+    private void debugTest(final CommandContextWrapper ctx) {
         ctx.sendMessage("Hello, world!");
+    }
+
+    @ModCommand(
+        description = "Displays a list of all current biome features.",
+        linter = ResourceArrayLinter.class,
+        branch = @Node(name = "feature", registry = Feature.class))
+    private void debugFeatures(final CommandContextWrapper ctx) {
+        final Feature<?> feature = ctx.get("feature", Feature.class);
+        final Stream<ResourceLocation> features = BuiltinRegistries.CONFIGURED_FEATURE.entrySet().stream()
+            .filter(e -> e.getValue().getFeatures().anyMatch(f -> feature.equals(f.feature)))
+            .map(e -> e.getKey().location());
+        ctx.sendLintedMessage(Arrays.toString(features.toArray()));
     }
 
     @ModCommand(
@@ -23,7 +42,7 @@ public class CommandOsv {
             "Removes all of the unnecessary loot tables from your ore presets.",
             "This will allow the variants to pull directly from their background blocks."
         })
-    private static void cleanupLoot(final CommandContextWrapper ctx) {
+    private void cleanupLoot(final CommandContextWrapper ctx) {
         final MutableInt count = new MutableInt(0);
         FileIO.listFilesRecursive(ModFolders.ORE_DIR, PresetLoadingContext::isPreset).forEach(preset ->
             HjsonUtils.updateJson(preset, json -> {
