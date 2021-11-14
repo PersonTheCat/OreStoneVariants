b11fefa8c6c6775b7da21869d8560b7b49eb179e
PersonTheCat <professionallyperson@gmail.com>
2021-11-14 13:39:07 -0600

    BlockSetRuleTest

---
+++ common/src/main/java/personthecat/osv/world/rule/BlockSetRuleTest.java
@@ -0,0 +1,41 @@
+package personthecat.osv.world.rule;
+
+import com.mojang.serialization.Codec;
+import net.minecraft.core.Registry;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTestType;
+import personthecat.catlib.data.InvertibleSet;
+import personthecat.catlib.serialization.EasyStateCodec;
+import personthecat.osv.util.Reference;
+
+import java.util.Collections;
+import java.util.Random;
+import java.util.Set;
+
+import static personthecat.catlib.serialization.CodecUtils.easySet;
+
+public class BlockSetRuleTest extends RuleTest {
+
+    public static final Codec<BlockSetRuleTest> CODEC = easySet(EasyStateCodec.INSTANCE)
+        .xmap(BlockSetRuleTest::new, rule -> rule.blocks);
+
+    public static final RuleTestType<BlockSetRuleTest> INSTANCE =
+        Registry.register(Registry.RULE_TEST, Reference.MOD_ID + ":block_set_rule_test", () -> CODEC);
+
+    private final Set<BlockState> blocks;
+
+    public BlockSetRuleTest(final Set<BlockState> blocks) {
+        this.blocks = new InvertibleSet<>(blocks, false).optimize(Collections.emptySet());
+    }
+
+    @Override
+    public boolean test(final BlockState state, final Random rand) {
+        return this.blocks.contains(state);
+    }
+
+    @Override
+    protected RuleTestType<?> getType() {
+        return INSTANCE;
+    }
+}
