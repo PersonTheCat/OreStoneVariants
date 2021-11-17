fea6b68e82b5e1fea62d3eaa4b34800bf136c9a8
PersonTheCat <professionallyperson@gmail.com>
2021-11-17 17:53:56 -0600

    Process Reference from properties

--- common/src/main/java/personthecat/osv/util/Reference.java
+++ common/src/main/java/personthecat/osv/util/Reference.java
@@ -1,23 +1,24 @@
 package personthecat.osv.util;
 
 import personthecat.catlib.data.ModDescriptor;
+import personthecat.catlib.versioning.Version;
 import personthecat.osv.io.ModFolders;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class Reference {
-    public static final String MOD_ID = "osv";
-    public static final String MOD_NAME = "Ore Stone Variants";
 
-    public static final ModDescriptor MOD_DESCRIPTOR = ModDescriptor.builder()
-        .modId(MOD_ID)
-        .name(MOD_NAME)
-        .preferredDirectory(ModFolders.ORE_DIR)
-        .build();
+    public static final String MOD_ID = "@MOD_ID@";
+    public static final String MOD_NAME = "@MOD_NAME@";
+    public static final Version MOD_VERSION = Version.parse("@MOD_VERSION@");
 
-    public static final List<String> SUPPORTED_MODS = Arrays
-        .asList("byg", "create", "druidcraft", "iceandfire", "minecraft", "osv", "simpleores", "quark");
+    public static final ModDescriptor MOD_DESCRIPTOR =
+        ModDescriptor.builder().modId(MOD_ID).name(MOD_NAME).version(MOD_VERSION)
+            .preferredDirectory(ModFolders.ORE_DIR).build();
+
+    public static final List<String> SUPPORTED_MODS =
+        Arrays.asList("byg", "create", "druidcraft", "iceandfire", "minecraft", "osv", "simpleores", "quark");
 
     public static final List<String> VALID_EXTENSIONS = Arrays.asList("json", "hjson");
 }
