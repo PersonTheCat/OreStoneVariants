968e87948ac25bc04e509f584f9d9e9551e9fa70
PersonTheCat <professionallyperson@gmail.com>
2021-11-24 09:50:11 -0600

    Sphere fixes

--- common/src/main/java/personthecat/osv/world/feature/VariantSphereFeature.java
+++ common/src/main/java/personthecat/osv/world/feature/VariantSphereFeature.java
@@ -2,6 +2,7 @@ package personthecat.osv.world.feature;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.feature.Feature;
 
@@ -17,31 +18,32 @@ public class VariantSphereFeature extends Feature<VariantSphereConfig> {
 
     @Override
     public boolean place(WorldGenLevel level, ChunkGenerator chunk, Random rand, BlockPos pos, VariantSphereConfig cfg) {
-        final int radX = cfg.radiusX.rand(rand);
-        final int radY = cfg.radiusY.rand(rand);
-        final int radZ = cfg.radiusZ.rand(rand);
-        final int radX2 = radX * radX;
-        final int radY2 = radY * radY;
-        final int radZ2 = radZ * radZ;
-        final int minY = Math.max(0, pos.getY() - radY);
-        final int maxY = Math.min(255, pos.getY() + radY);
+        final double radX = cfg.radiusX.rand(rand);
+        final double radY = cfg.radiusY.rand(rand);
+        final double radZ = cfg.radiusZ.rand(rand);
+        final double radX2 = radX * radX;
+        final double radY2 = radY * radY;
+        final double radZ2 = radZ * radZ;
+        final int minY = Math.max(0, pos.getY() - (int) radY);
+        final int maxY = Math.min(255, pos.getY() + (int) radY);
+        final BlockPos.MutableBlockPos mutable = pos.mutable();
         int count = 0;
 
-        for (int x = pos.getX() - radX; x < pos.getX() + radX; x++) {
-            final int distX = x - pos.getX();
-            final int distX2 = distX * distX;
+        for (int x = pos.getX() - (int) radX; x <= pos.getX() + (int) radX; x++) {
+            final double distX = x - pos.getX();
+            final double distX2 = distX * distX;
 
-            for (int z = pos.getZ() - radZ; x < pos.getZ() + radZ; z++) {
-                final int distZ = z - pos.getZ();
-                final int distZ2 = distZ * distZ;
+            for (int z = pos.getZ() - (int) radZ; z <= pos.getZ() + (int) radZ; z++) {
+                final double distZ = z - pos.getZ();
+                final double distZ2 = distZ * distZ;
 
-                for (int y = minY; y < maxY; y++) {
-                    final int distY = y - pos.getY();
-                    final int distY2 = distY * distY;
+                for (int y = minY; y <= maxY; y++) {
+                    final double distY = y - pos.getY();
+                    final double distY2 = distY * distY;
 
-                    if (distX2 / radX2 + distY2 / radY2 + distZ2 / radZ2 <= 1) {
-                        if (cfg.integrity == 1 || rand.nextDouble() <= cfg.integrity) {
-                            if (BlockMatchingSpawnConfig.tryPlace(cfg.blocks, rand, level, pos)) {
+                    if (distX2 / radX2 + distY2 / radY2 + distZ2 / radZ2 <= 1.0) {
+                        if (cfg.integrity == 1.0 || rand.nextDouble() <= cfg.integrity) {
+                            if (BlockMatchingSpawnConfig.tryPlace(cfg.blocks, rand, level, mutable.set(x, y, z))) {
                                 count++;
                             }
                         }
