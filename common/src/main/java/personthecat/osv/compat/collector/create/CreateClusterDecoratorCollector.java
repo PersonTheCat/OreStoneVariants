49a371ec19e08776bbd686379fadc0cb5371c25e
PersonTheCat <professionallyperson@gmail.com>
2021-12-10 18:39:07 -0600

    Add missing annotation

--- common/src/main/java/personthecat/osv/compat/collector/create/CreateClusterDecoratorCollector.java
+++ common/src/main/java/personthecat/osv/compat/collector/create/CreateClusterDecoratorCollector.java
@@ -1,5 +1,6 @@
 package personthecat.osv.compat.collector.create;
 
+import dev.architectury.injectables.annotations.ExpectPlatform;
 import personthecat.osv.compat.collector.DecoratorCollector;
 import personthecat.osv.preset.data.FlexibleDecoratorSettings;
 import personthecat.osv.preset.data.FlexibleDecoratorSettings.FlexibleDecoratorSettingsBuilder;
@@ -12,6 +13,7 @@ public abstract class CreateClusterDecoratorCollector extends DecoratorCollector
         super(FlexibleDecoratorSettings::builder, FlexibleDecoratorSettingsBuilder::build);
     }
 
+    @ExpectPlatform
     public static Optional<CreateClusterDecoratorCollector> getInstance() {
         throw new AssertionError();
     }
