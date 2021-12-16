c4ca00c7080f52c024f83af5954f64b0b0309884
PersonTheCat <professionallyperson@gmail.com>
2021-12-16 10:07:16 -0600

    Render in transparent layer for overlay models

--- platforms/forge/src/main/java/personthecat/osv/client/forge/VariantRenderDispatcherImpl.java
+++ platforms/forge/src/main/java/personthecat/osv/client/forge/VariantRenderDispatcherImpl.java
@@ -4,12 +4,16 @@ import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import personthecat.osv.block.OreVariant;
 import personthecat.osv.config.VariantDescriptor;
+import personthecat.osv.preset.data.ModelSettings;
 
 public class VariantRenderDispatcherImpl {
 
     @SuppressWarnings("deprecation")
     public static void setupRenderLayer(final VariantDescriptor descriptor, final OreVariant variant) {
         final RenderType layer = ItemBlockRenderTypes.getChunkRenderType(variant.getBg().defaultBlockState());
+        if (descriptor.getForeground().getModel().getType() == ModelSettings.Type.OVERLAY) {
+            ItemBlockRenderTypes.setRenderLayer(variant, l -> l == layer || l == RenderType.translucent());
+        }
         ItemBlockRenderTypes.setRenderLayer(variant, layer);
     }
 }
