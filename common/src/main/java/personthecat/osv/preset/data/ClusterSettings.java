18ff1a0794f79fa9b77bed0752ca0a1861fe580b
PersonTheCat <professionallyperson@gmail.com>
2021-12-18 21:52:30 -0600

    Migrate to `osv:cluster` for stone clusters

--- common/src/main/java/personthecat/osv/preset/data/ClusterSettings.java
+++ common/src/main/java/personthecat/osv/preset/data/ClusterSettings.java
@@ -6,13 +6,12 @@ import lombok.Builder.Default;
 import lombok.Value;
 import lombok.experimental.FieldNameConstants;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import personthecat.osv.preset.OrePreset;
 import personthecat.osv.preset.StonePreset;
 import personthecat.osv.world.feature.ClusterConfig;
 import personthecat.osv.world.feature.ClusterFeature;
 import personthecat.osv.world.feature.FeatureProvider;
+import personthecat.osv.world.placer.StoneBlockPlacer;
 import personthecat.osv.world.placer.VariantBlockPlacer;
 
 import static personthecat.catlib.serialization.CodecUtils.codecOf;
@@ -37,7 +36,7 @@ public class ClusterSettings implements FeatureProvider<ClusterSettings> {
 
     @Override
     public ConfiguredFeature<?, ?> createStoneFeature(final StonePreset stone, final DecoratedFeatureSettings<?, ?> cfg) {
-        return Feature.ORE.configured(new OreConfiguration(stone.getSource(), stone.getStone(), this.size));
+        return ClusterFeature.INSTANCE.configured(new ClusterConfig(this.size, new StoneBlockPlacer(stone)));
     }
 
     @Override
